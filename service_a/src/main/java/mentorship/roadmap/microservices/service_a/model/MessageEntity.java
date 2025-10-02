a86634dea5d2b7cbb3f4e053f1c80080b898faf1
dornesk <dornesk@icloud.com>
2025-10-02 15:23:46 +0300

    Added MessageEntity class, MessageType enum

---
+++ service_a/src/main/java/mentorship/roadmap/microservices/service_a/model/MessageEntity.java
@@ -0,0 +1,25 @@
+package mentorship.roadmap.microservices.service_a.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "messages")
+public class MessageEntity {
+
+    @Id
+    private int id;
+
+    @NonNull
+    private MessageType type;
+
+    @NonNull
+    private String content;
+}
---
+++ service_a/src/main/java/mentorship/roadmap/microservices/service_a/model/MessageType.java
@@ -0,0 +1,7 @@
+package mentorship.roadmap.microservices.service_a.model;
+
+public enum MessageType {
+    IMPORTANT,
+    NORMAL,
+    OTHER
+}
